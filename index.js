85a808ae066032eaaa63bfd77cb8ba3003f94d26
Richie Casto <rcasto92@gmail.com>
2020-06-12 22:23:16 -0700

    updating to pass in config and make into npm package

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
+const path = require('path');
 const promisify = require('util').promisify;
 const readFilePromise = promisify(require('fs').readFile);
 const execPromise = promisify(require('child_process').exec);
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
-const config = require('./config.json');
+
+/**
+ * @typedef {Object} SmtpConfig
+ * @property {string} host
+ * @property {number} port
+ * @property {string} username
+ * @property {string} password
+ */
+
+/**
+ * @typedef {Object} PiUpConfig
+ * @property {string} [scheduleCron] crontab describing schedule of which to run update script.
+ * @property {boolean} [runOnInit]
+ * @property {string} [scriptPath]
+ * @property {string} [email]
+ * @property {string} [name]
+ * @property {SmtpConfig} [smtpInfo]
+ */
 
 const testScriptExecutionArgument = 'testing';
-const transporter = nodemailer.createTransport({
-    host: config.smtpInfo.host,
-    port: config.smtpInfo.port,
-    secure: false,
-    auth: {
-        user: config.smtpInfo.username,
-        pass: config.smtpInfo.password
+/**
+ * @type {PiUpConfig}
+ */
+const defaultConfig = Object.freeze({
+    scheduleCron: '',
+    runOnInit: true,
+    scriptPath: path.resolve(__dirname, './custom-update.sh'),
+    email: '',
+    name: '',
+    smtpInfo: {
+        host: '',
+        port: 587,
+        username: '',
+        password: ''
     }
 });
 
+/**
+ * @param {string} scriptPath
+ * @returns {Promise<string>} The custom script file contents as a string
+ */
 async function readCustomScript(scriptPath) {
     try {
         const customScript = await readFilePromise(scriptPath, {
@@ -27,31 +56,50 @@ async function readCustomScript(scriptPath) {
     }
 }
 
-async function sendMail(emailAddress, emailText, name) {
+/**
+ * @param {string} emailText 
+ * @param {PiUpConfig} config
+ * @returns {Promise<void>}
+ */
+async function sendMail(emailText, config) {
     if (!config.email) {
         console.log(`No email address provided, not sending email.`);
         return;
     }
 
     try {
+        const transporter = nodemailer.createTransport({
+            host: config.smtpInfo.host,
+            port: config.smtpInfo.port,
+            secure: false,
+            auth: {
+                user: config.smtpInfo.username,
+                pass: config.smtpInfo.password
+            }
+        });
         const message = {
-            from: `pi-up <${emailAddress}>`,
-            to: emailAddress,
-            subject: name ?
-                `${name} - pi-up results` :
+            from: `pi-up <${config.email}>`,
+            to: config.email,
+            subject: config.name ?
+                `${config.name} - pi-up results` :
                 `pi-up results`,
             text: emailText,
         };
 
         await transporter.sendMail(message);
 
-        console.log(`Successfully sent email to ${emailAddress}`);
+        console.log(`Successfully sent email to ${config.email}`);
     } catch (err) {
-        console.error(`Failed to send email to ${emailAddress}\n${err}`);
+        console.error(`Failed to send email to ${config.email}\n${err}`);
     }
 }
 
-async function onSchedule(customScript, emailAddress, name = '') {
+/**
+ * @param {string} customScript 
+ * @param {PiUpConfig} config 
+ * @returns {Promise<void>}
+ */
+async function onSchedule(customScript, config) {
     let emailText;
 
     try {
@@ -82,17 +130,26 @@ async function onSchedule(customScript, emailAddress, name = '') {
         emailText = `An error occurred while executing the custom script\n${err}`;
         console.error(emailText);
     } finally {
-        await sendMail(emailAddress, emailText, name);
+        await sendMail(emailText, config);
     }
 }
 
-async function onInit() {
+/**
+ * 
+ * @param {PiUpConfig} [config] Optional configuration for overriding pi-up defaults
+ */
+async function onInit(config = defaultConfig) {
+    config = {
+        ...defaultConfig,
+        ...config
+    };
+
     const customScript = await readCustomScript(config.scriptPath);
     const isTestScriptExecution = process.argv.slice(2)
         .some(arg => arg === testScriptExecutionArgument);
 
     if (isTestScriptExecution || config.runOnInit) {
-        await onSchedule(customScript, config.email, config.name);
+        await onSchedule(customScript, config);
 
         if (isTestScriptExecution) {
             // Only testing the script out once, no need to start
@@ -101,11 +158,13 @@ async function onInit() {
         }
     }
 
-    console.log(`Starting schedule: ${config.scheduleCron}`);
-
-    const scheduledJob = new CronJob(config.scheduleCron,
-        () => onSchedule(customScript, config.email, config.name));
-    scheduledJob.start();
+    if (config.scheduleCron) {
+        console.log(`Starting schedule: ${config.scheduleCron}`);
+        
+        const scheduledJob = new CronJob(config.scheduleCron,
+            () => onSchedule(customScript, config));
+        scheduledJob.start();
+    }
 }
 
-onInit();
+module.exports = onInit;
