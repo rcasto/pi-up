e86bd4ad51439a33fede710b9b7fb66fed77aa07
Richie Casto <rcasto92@gmail.com>
2020-06-12 22:21:06 -0700

    add test client

---
+++ test/index.js
@@ -0,0 +1,17 @@
+const piUp = require('pi-up');
+
+piUp();
+
+// piUp({
+//     "scheduleCron": "0 0 18 * * *",
+//     "runOnInit": true,
+//     "scriptPath": "./custom-update.sh",
+//     "email": "",
+//     "name": "",
+//     "smtpInfo": {
+//         "host": "",
+//         "port": 587,
+//         "username": "",
+//         "password": ""
+//     }
+// });
